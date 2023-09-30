1648ea9919f5430437361caa7447ed0f97529077
Jonathan <jonathan.ibanez@cua.uam.mx>
2023-09-29 18:59:45 -0600

    Se agrego el splash inicial

--- Assets/MenuInicial.cs
+++ Assets/MenuInicial.cs
@@ -2,9 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 public class MenuInicial : MonoBehaviour
 {
+    public VideoPlayer intro;
+    public GameObject menuInicial;
+
+    private void Awake()
+    {
+        menuInicial.SetActive(false); 
+        intro = GetComponent<VideoPlayer>();
+        intro.Play();
+        intro.loopPointReached += PasarMenu;
+         
+    }
+
+    void PasarMenu(VideoPlayer vp)
+    {
+        gameObject.SetActive(false);
+        menuInicial.SetActive(true);
+    }
+
     public void Jugar()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
